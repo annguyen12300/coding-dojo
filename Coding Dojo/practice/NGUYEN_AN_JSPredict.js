a01bc331fd5c4baf640ec1e00ca93cc902cee988
An Nguyen <91802029+annguyen12300@users.noreply.github.com>
2021-10-10 14:31:50 -0700

    Add files via upload

---
+++ Coding Dojo/practice/NGUYEN_AN_JSPredict.js
@@ -0,0 +1,3 @@
+//Predic 1: console.log will state the born year plus 1980 for the result
+//Predict 2: console.log will state the year that born without adding the 1980
+//Predict 3: console.log will state 30
